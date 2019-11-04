035bebc6d4cb6700d35cb38b53f80d7988fb8c1c
GamerPsy <ducry.remy@gmail.com>
2019-11-04 15:31:34 +0100

    ajout section compétences

--- skills.php
+++ skills.php
@@ -1,5 +1,249 @@
 <?php include "include/head.html" ?>
 
-<p class =" bande-annonce incoming">Partie compétences en construction !</p>
+<main class="bg-dark">
+    <div class="container">
+        <!-------------------------------------   SECTION BACK  --------------------------------------------------->
+        <div class="row bg-info">
+            <div class="col-12">
+                <h3 class="border-bottom text-center pb2">Back-end</h3>
+            </div>
+            <!----------------- PHP ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_PHP.png" alt="PHP 7"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        PHP 7
+                    </div>
+                </div>
+            </div>
+            <!----------------- SYMFONY ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/frameworks/Logo_symfony.png" alt="Symfony 4"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        SYMFONY
+                    </div>
+                </div>
+            </div>
+            <!----------------- SQL ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_SQL.png" alt="SQL"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        SQL
+                    </div>
+                </div>
+            </div>
+            <!----------------- Python ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_python.png" alt="Python"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Python
+                    </div>
+                </div>
+            </div>
+            <!----------------- PERL ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_PERL.png" alt="PERL"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        PERL
+                    </div>
+                </div>
+            </div>
+            <!----------------- JQuery ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/frameworks/Logo_JQUERY.png" alt="JQUERY"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        JQUERY
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-------------------------------------   SECTION FRONT  -------------------------------------------------->
+        <div class="row bg-danger">
+            <div class="col-12">
+                <h3 class="border-bottom text-center pb2">Front-end</h3>
+            </div>
+            <!----------------- HTML ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_HTML5.png" alt="HTML 5"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        HTML 5
+                    </div>
+                </div>
+            </div>
+            <!----------------- CSS ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_CSS3.png" alt="CSS 3"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        CSS 3
+                    </div>
+                </div>
+            </div>
+            <!----------------- JAVASCRIPT ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_JS.png" alt="Javascript"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        JAVASCRIPT
+                    </div>
+                </div>
+            </div>
+            <!----------------- TWIG ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/outils/Logo_twig.png" alt="TWIG"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        TWIG
+                    </div>
+                </div>
+            </div>
+            <!----------------- BOOTSTRAP ------------------->
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/frameworks/bootstrap-4.png" alt="Bootstrap 4"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        BOOTSTRAP 4
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-------------------------------------   SECTION AUTRE  -------------------------------------------------->
+        <div class="row bg-warning">
+            <div class="col-12">
+                <h3 class="border-bottom text-center pb2">Autre</h3>
+            </div>
+
+            <!----------------- Git ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/outils/Logo_git.png" alt="Git"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Git
+                    </div>
+                </div>
+            </div>
+            <!----------------- GITHUB ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/outils/Logo_github.png" alt="Github"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Github
+                    </div>
+                </div>
+            </div>
+            <!----------------- Doctrine ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/outils/Logo_doctrine.png" alt="Doctrine"/></p>
+                        <p class="card-text text-light">Niveau : confirmé</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Doctrine
+                    </div>
+                </div>
+            </div>
+            <!----------------- Koha ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/outils/Logo_Koha.png" alt="Koha"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Koha
+                    </div>
+                </div>
+            </div>
+            <!----------------- Lua ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/langages/Logo_lua.png" alt="Lua"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Lua
+                    </div>
+                </div>
+            </div>
+            <!----------------- Love2D ------------------------>
+            <div class="col-2 pb-2">
+                <div class="card text-center bg-dark h-100">
+                    <div class="card-body">
+                        <p class="effect rounded mx-auto d-block img-fluid"><img
+                                    src="images/frameworks/Love2D.png" alt="LOVE 2D"/></p>
+                        <p class="card-text text-light">Niveau : débutant</p>
+                    </div>
+                    <div class="card-footer font-weight-bold text-white">
+                        Love 2D
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
 
 <?php include "include/footer.html" ?>
