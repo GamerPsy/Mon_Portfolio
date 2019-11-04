cb7f7f0f539c0cb5b2065b40569ac7d2bd73e095
GamerPsy <ducry.remy@gmail.com>
2019-11-04 17:52:06 +0100

    sauvegarde avancée du jour

--- projets.php
+++ projets.php
@@ -1,5 +1,149 @@
 <?php include "include/head.html" ?>
 
-<p class ="bande-annonce incoming">Partie projets en construction !</p>
+<main class="bg-dark">
+    <div class="container">
+        <!-------------------------------------   SECTION 2019  -------------------------------------------------->
+        <div class="row">
+            <div class="col-12">
+                <h3 class="border-bottom text-center pb2">2019</h3>
+            </div>
+
+            <!----------------- Mon portfolio ------------------------>
+            <div class="col-6 pb-2">
+                <div class="card bg-success">
+                    <div class="card-header">
+                        <h3 class="text-center">Projet perso : mon portfolio</h3>
+                    </div>
+                    <img class="card-img-top" src="images/portfolio.png" alt="Mon Portfolio"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Mon portfolio</h5>
+                        <p class="card-text">
+                            Conception et réalisation de mon portfolio en ligne.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">PHP 7</span>
+                            <span class="badge badge-info">HTML 5</span>
+                            <span class="badge badge-info">CSS 3</span>
+                            <span class="badge badge-info">Bootstrap 4</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href="https://github.com/GamerPsy/Mon_Portfolio"
+                           title="Mon portfolio" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> 1 personne ( moi-même ! )
+                    </div>
+                </div>
+            </div>
+            <!----------------- WCS Projet 3 - Application pour client ------------------------>
+            <div class="col-6 pb-2">
+                <div class="card color-wcs">
+                    <div class="card-header">
+                        <h3 class="text-center">Projet 3 à la Wild Code School</h3>
+                    </div>
+                    <img class="card-img-top" src="images/projet3.png" alt="Simulateur_Pinel"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Simulateur Pinel</h5>
+                        <p class="card-text">
+                            Conception et réalisation d'un site web pour une association.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">Symfony 4</span>
+                            <span class="badge badge-info">PHP 7</span>
+                            <span class="badge badge-info">Doctrine</span>
+                            <span class="badge badge-info">Twig</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href="https://github.com/GamerPsy/WCS_Projet3_SimulateurLoiPinel"
+                           title="Simulateur_Pinel" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-clock mr-2"></i> 8 semaines
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> 5 personnes
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-------------------------------------   SECTION 2018  -------------------------------------------------->
+        <div class="row">
+            <div class="col-12">
+                <h3 class="border-bottom text-center pb2">2018</h3>
+            </div>
+
+            <!----------------- WCS Projet 2 - Site dynamique ------------------------>
+            <div class="col-6 pb-2">
+                <div class="card color-wcs">
+                    <div class="card-header">
+                        <h3 class="text-center">Projet 2 à la Wild Code School</h3>
+                    </div>
+                    <img class="card-img-top" src="images/projet2.gif" alt="WCS-ASLO45"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Association Sabres laser</h5>
+                        <p class="card-text">
+                            Conception et réalisation d'un site web pour une association.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">HTML 5</span>
+                            <span class="badge badge-info">CSS 3</span>
+                            <span class="badge badge-info">Bootstrap 4</span>
+                            <span class="badge badge-info">PHP 7</span>
+                            <span class="badge badge-info">SQL</span>
+                            <span class="badge badge-info">MVC</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href="https://github.com/GamerPsy/WCS_Projet2_ASLO45"
+                           title="WCS-ASLO45" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-clock mr-2"></i> 5 semaines
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> 5 personnes
+                    </div>
+                </div>
+            </div>
+        <!----------------- WCS Projet 1 - Intégration ------------------------>
+        <div class="col-6 pb-2">
+            <div class="card color-wcs">
+                <div class="card-header">
+                    <h3 class="text-center">Projet 1 à la Wild Code School</h3>
+                </div>
+                <img class="card-img-top" src="images/projet1.gif" alt="WCS-Projet1_JourneeSansChaussures"/>
+                <div class="card-body">
+                    <h5 class="card-title">Journée mondiale du travail sans chaussures</h5>
+                    <p class="card-text">
+                        Conception et réalisation d'un site web pour une association.
+                    </p>
+                    <p class="card-skills">
+                        <span class="badge badge-info">HTML 5</span>
+                        <span class="badge badge-info">CSS 3</span>
+                        <span class="badge badge-info">Bootstrap 4</span>
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <a role="button" class="btn btn-secondary"
+                       href="https://github.com/GamerPsy/WCS_Projet1_JourneeSansChaussures"
+                       title="WCS-Projet1_JourneeSansChaussures" target="_blank">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <span class="mx-2">|</span>
+                    <i class="fas fa-clock mr-2"></i> 3 semaines
+                    <span class="mx-2">|</span>
+                    <i class="fas fa-user mr-2"></i> 3 personnes
+                </div>
+            </div>
+        </div>
+</main>
 
 <?php include "include/footer.html" ?>
