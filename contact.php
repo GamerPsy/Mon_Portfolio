3ae970a82cab71e78cdc3a6cf55eb1b5f78cd04f
GamerPsy <ducry.remy@gmail.com>
2019-11-05 21:59:20 +0100

    amélioration front contact

--- contact.php
+++ contact.php
@@ -8,23 +8,31 @@
                 <div class="card h-100">
                     <h2 class="mx-auto">Formulaire de contact</h2>
                     <div class="card-body">
-                        <form method="post" action="contact.php" >
+                        <form method="post" action="contact.php">
                             <div class="row mx-auto">
-                                <label for="nom">Nom :</label>
-                                <input class="col-9 mx-auto" type="text" id="nom" name="user_nom" required/>
+                                <div class="col-12">
+                                    <label for="nom">Nom et Prénom :</label>
+                                </div>
+                                <input class="col-12 mx-auto" type="text" id="nom" name="user_nom"
+                                       placeholder="DOE John" required/>
                             </div>
                             <p><?php if (isset($errors['name1'])) echo $errors['name1']; ?></p>
 
                             <div class="row mx-auto">
-                                <label for="mail">e-mail :</label>
-                                <input type="email" id="mail" class="col-9 mx-auto" name="user_mail" required/>
+                                <div class="col-12">
+                                    <label for="mail">e-mail :</label>
+                                </div>
+                                <input type="email" id="mail" class="col-12 mx-auto" name="user_mail"
+                                       placeholder="contact@me.fr" required/>
                             </div>
                             <p><?php if (isset($errors['mail1'])) echo $errors['mail1']; ?></p>
                             <p><?php if (isset($errors['mail2'])) echo $errors['mail2']; ?></p>
 
                             <div class="row mx-auto">
-                                <label for="msg">Votre message :</label>
-                                <textarea id="msg" class="col-9" name="user_message"
+                                <div class="col-12">
+                                    <label for="msg">Votre message :</label>
+                                </div>
+                                <textarea id="msg" class="col-12" name="user_message"
                                           placeholder="Exprimez vous !"
                                           required/></textarea>
                             </div>
