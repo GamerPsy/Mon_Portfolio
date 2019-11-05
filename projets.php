98e32732d2d11337282ca8be2f613ba5f8e0be63
GamerPsy <ducry.remy@gmail.com>
2019-11-05 14:24:58 +0100

    maj de la partie projets

--- projets.php
+++ projets.php
@@ -2,15 +2,111 @@
 
 <main class="bg-dark">
     <div class="container">
+        <!-------------------------------------   SECTION DU FUTUR  -------------------------------------------------->
+        <div class="row">
+            <div class="col-12">
+                <button type="button" class="btn btn-secondary btn-lg btn-block mb-3" disabled>
+                    <h3 class="border-bottom text-center pb2">Projets à venir !</h3>
+                </button>
+            </div>
+            <!----------------- Apprentissage : JAVA JEE ------------------------>
+            <div class="col-4 pb-2">
+                <div class="card bg-success h-100">
+                    <div class="card-header">
+                        <h3 class="text-center">Java</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Je vais me lancer dans l'apprentissage de ce langage via la plateforme d'enseignement de la
+                            Wild Code School (Odyssey) et compléter si besoin. Je créerai une repository avec plusieurs
+                            branches sur Github pour la correction des quêtes.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">Apprentissage</span>
+                            <span class="badge badge-info">Développeur</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href=""
+                           title="Ma progression en Java" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> Moi-même !
+                    </div>
+                </div>
+            </div>
+            <!----------------- Apprentissage : Docker ------------------------>
+            <div class="col-4 pb-2">
+                <div class="card bg-success h-100">
+                    <div class="card-header">
+                        <h3 class="text-center">Docker</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Mon stage avec le PREBS m'a fait prendre conscience de l'aspect devOps qu'il y a dans tout
+                            process pour qu'un site ou une application soit disponible sur internet. Du coup c'est un
+                            apprentissage qui me parait logique pour moi.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">Apprentissage</span>
+                            <span class="badge badge-info">DevOps</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href=""
+                           title="Docker" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> Moi-même !
+                    </div>
+                </div>
+            </div>
+            <!----------------- Jeux vidéos : plateformer avec dinosaures ------------------------>
+            <div class="col-4 pb-2">
+                <div class="card bg-success h-100">
+                    <div class="card-header">
+                        <h3 class="text-center">Dino Jungle</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Projet pour m'initier à la programmation de jeu vidéo, une idée assez simple mais qui
+                            me permettra de toucher à des concepts de collisions, scores et peut-être même une IA pour
+                            les ennemis.
+                        </p>
+                        <p class="card-skills">
+                            <span class="badge badge-info">Lua</span>
+                            <span class="badge badge-info">Love 2D</span>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a role="button" class="btn btn-secondary"
+                           href=""
+                           title="Dino Jungle" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <span class="mx-2">|</span>
+                        <i class="fas fa-user mr-2"></i> Moi-même !
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
         <!-------------------------------------   SECTION 2019  -------------------------------------------------->
         <div class="row">
             <div class="col-12">
-                <h3 class="border-bottom text-center pb2">2019</h3>
+                <button type="button" class="btn btn-secondary btn-lg btn-block mb-3" disabled>
+                    <h3 class="border-bottom text-center pb2"> Projets de l'année 2019</h3>
+                </button>
             </div>
 
             <!----------------- Mon portfolio ------------------------>
             <div class="col-6 pb-2">
-                <div class="card bg-success">
+                <div class="card bg-success h-100">
                     <div class="card-header">
                         <h3 class="text-center">Projet perso : mon portfolio</h3>
                     </div>
@@ -18,7 +114,8 @@
                     <div class="card-body">
                         <h5 class="card-title">Mon portfolio</h5>
                         <p class="card-text">
-                            Conception et réalisation de mon portfolio en ligne.
+                            Conception et réalisation de mon portfolio en ligne. Me permet de revoir mes bases et
+                            d'utiliser mes compétences pour mon propre compte ^^
                         </p>
                         <p class="card-skills">
                             <span class="badge badge-info">PHP 7</span>
@@ -34,13 +131,13 @@
                             <i class="fab fa-github"></i>
                         </a>
                         <span class="mx-2">|</span>
-                        <i class="fas fa-user mr-2"></i> 1 personne ( moi-même ! )
+                        <i class="fas fa-user mr-2"></i> Moi-même !
                     </div>
                 </div>
             </div>
             <!----------------- WCS Projet 3 - Application pour client ------------------------>
             <div class="col-6 pb-2">
-                <div class="card color-wcs">
+                <div class="card color-wcs h-100">
                     <div class="card-header">
                         <h3 class="text-center">Projet 3 à la Wild Code School</h3>
                     </div>
@@ -48,7 +145,8 @@
                     <div class="card-body">
                         <h5 class="card-title">Simulateur Pinel</h5>
                         <p class="card-text">
-                            Conception et réalisation d'un site web pour une association.
+                            Conception et réalisation d'une application métier à l'usage des gestionnaires de biens
+                            immobiliser pour un simulateur loi Pinel.
                         </p>
                         <p class="card-skills">
                             <span class="badge badge-info">Symfony 4</span>
@@ -73,7 +171,7 @@
             <!----------------- Hackathon 2 - Reporting Lab'O ------------------------>
             <!--
             <div class="col-6 pb-2">
-                <div class="card bg-danger">
+                <div class="card bg-danger h-100">
                     <div class="card-header">
                         <h3 class="text-center">Hackathon : Reporting Lab'O</h3>
                     </div>
@@ -110,12 +208,14 @@
         <!-------------------------------------   SECTION 2018  -------------------------------------------------->
         <div class="row">
             <div class="col-12">
-                <h3 class="border-bottom text-center pb2">2018</h3>
+                <button type="button" class="btn btn-secondary btn-lg btn-block mb-3" disabled>
+                    <h3 class="border-bottom text-center pb2">Projets de l'année 2018</h3>
+                </button>
             </div>
 
             <!----------------- WCS Projet 2 - Site dynamique ------------------------>
             <div class="col-6 pb-2">
-                <div class="card color-wcs">
+                <div class="card color-wcs h-100">
                     <div class="card-header">
                         <h3 class="text-center">Projet 2 à la Wild Code School</h3>
                     </div>
@@ -123,7 +223,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Association Sabres laser</h5>
                         <p class="card-text">
-                            Conception et réalisation d'un site web pour une association.
+                            Refonte du site de l'Académie de Sabre Laser Orléanaise.
                         </p>
                         <p class="card-skills">
                             <span class="badge badge-info">HTML 5</span>
@@ -150,7 +250,7 @@
             <!----------------- Hackathon 1 - Bonbondex ------------------------>
             <!--
             <div class="col-6 pb-2">
-                <div class="card bg-danger">
+                <div class="card bg-danger h-100">
                     <div class="card-header">
                         <h3 class="text-center">Hackathon : Bonbondex</h3>
                     </div>
@@ -185,7 +285,7 @@
             -->
             <!----------------- WCS Projet 1 - Intégration ------------------------>
             <div class="col-6 pb-2">
-                <div class="card color-wcs">
+                <div class="card color-wcs h-100">
                     <div class="card-header">
                         <h3 class="text-center">Projet 1 à la Wild Code School</h3>
                     </div>
